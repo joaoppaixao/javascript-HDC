254870ba5df5371b327cbf2b0e645e8be41c6965
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-01-09 15:07:47 -0300

    tipos de dados e operadores em js

---
+++ 15_operadores/js/script.js
@@ -0,0 +1,17 @@
+// 1 - Number
+console.log(typeof 2);
+
+// 2 - Operações Aritméticas
+console.log(12 + 14);
+console.log(24 - 12);
+console.log(6 * 2);
+console.log(52 / 2);
+
+// 3 - Special numbers
+console.log(typeof Infinity);
+
+console.log(typeof -Infinity);
+
+console.log(12 * "bru");
+
+console.log(typeof NaN);
