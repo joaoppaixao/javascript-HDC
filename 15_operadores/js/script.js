431e3e03f6bee83ba21d22d9f37ec821a84b6114
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-01-13 12:06:11 -0300

    finalizei os operadores logicos em js

--- 15_operadores/js/script.js
+++ 15_operadores/js/script.js
@@ -15,3 +15,87 @@ console.log(typeof -Infinity);
 console.log(12 * "bru");
 
 console.log(typeof NaN);
+
+// 4 - Strings
+console.log("Um texto");
+console.log(`Mais um texto`);
+console.log("26");
+
+console.log(typeof "Um texto");
+console.log(typeof `Mais um texto`);
+
+// 5 - Símbolos especiais em String
+console.log("Testando a \n quebra de linha");
+console.log("Espaçamento de \t tab");
+
+// 6 - Concatenação
+console.log("Oi, " + "tudo " + "bem?");
+
+// 7 - Interpolação
+console.log(`A soma de 2 + 2 é: ${2 + 2}`);
+
+// 8 - Boolean
+console.log(true);
+
+console.log( 5 > 20);
+
+console.log(26 > 12);
+
+console.log(typeof true);
+
+// 9 - Comparações
+console.log(5 <= 5);
+
+console.log(5 < 5);
+
+console.log(12 == 12);
+
+console.log(26 == 12);
+
+console.log(26 != 12);
+
+// 10 - Idêntico
+console.log(9 == "9");
+
+console.log(9 === "9");
+
+console.log(9 != "9");
+
+console.log(9 !== "9");
+
+// 11 - Operadores lógicos
+console.log(true && true);
+
+console.log(true && false);
+
+console.log(26 > 12 && 12 < 26);
+
+console.log(12 < 26 && "Bruna" === 12);
+
+console.log(12 < 26 || "Bruna" === 12);
+
+console.log(12 > 26 || 26 < 12);
+
+console.log(!true);
+
+console.log(!false);
+
+// 12 - Empty values
+console.log(typeof null, typeof undefined);
+
+console.log(null === undefined);
+
+console.log(null == undefined);
+
+console.log(null == false);
+
+console.log(undefined == false);
+
+// 13 - Mudança de tipos
+console.log(5 * null);
+
+console.log("teste" * "opa");
+
+console.log("10" + 1);
+
+console.log("10" - 1);
