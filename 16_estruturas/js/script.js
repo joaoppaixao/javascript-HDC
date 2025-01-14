ade9425040e8f41c0d05f4ef14626f265e40725a
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-01-14 13:44:33 -0300

    iniciei o modulo de estruturas de programaçao

---
+++ 16_estruturas/js/script.js
@@ -0,0 +1,43 @@
+// 1 - Variáveis
+let nome = "Bruna";
+
+console.log(nome);
+
+nome = "Bruna Moreira Marvulle";
+
+console.log(nome);
+
+const idade = 19;
+
+console.log(idade);
+
+// idade = 20; (não é possível mudar o valor da variável const)
+
+console.log(typeof nome);
+
+console.log(typeof idade);
+
+// 2 - Mais sobre variáveis
+let a = 10, b = 20, c = 30;
+
+console.log(a, b, c);
+
+let nomecompleto = "João Augusto Palma Paixão";
+
+let nomeCompleto = "Bruna Moreira Marvulle";
+
+console.log(nomecompleto);
+
+console.log(nomeCompleto);
+
+// 3 - Prompt
+// const age = prompt("Digite a sua idade: ");
+
+// console.log(`Você tem ${age} anos!`);
+
+// 4 - Alert
+alert("Testando");
+
+const z = 12;
+
+alert(`O número é ${z} `)
