f6d99a778bfdc41befd032749ae18b8d0d2a36c0
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-01-16 13:01:25 -0300

    modulo de funçoes iniciado

---
+++ 17_funcoes/js/script.js
@@ -0,0 +1,49 @@
+// 1 - criando uma função
+
+function minhaFuncao() {
+    console.log("Testando")
+}
+
+minhaFuncao();
+
+const minhaFuncaoEmVariavel = function() {
+    console.log("Função em variável.")
+}
+
+minhaFuncaoEmVariavel();
+
+function funcaoComParametro(txt) {
+    console.log(`Imprimindo: ${txt}`)
+}
+
+funcaoComParametro("Imprimindo alguma coisa...");
+
+funcaoComParametro("outra função.");
+
+// 2 - return
+const a = 10;
+const b = 20;
+const c = 30;
+const d = 40;
+
+function soma(n1, n2) {
+    return n1 + n2;
+}
+
+const resultado = soma(a, b);
+
+console.log(resultado);
+
+console.log(soma(c, d));
+
+// 3 - escopo da função
+let y = 10;
+
+function testandoEscopo() {
+    let y = 20;
+    console.log(`Y dentro da função é: ${y}`);
+}
+
+console.log(`Y fora da função é: ${y}`);
+
+testandoEscopo();
