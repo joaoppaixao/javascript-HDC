f39ddfa48c0155d1ce841121b6efb69fc283b07d
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-01-16 14:39:48 -0300

    teste

--- 17_funcoes/js/script.js
+++ 17_funcoes/js/script.js
@@ -47,3 +47,20 @@ function testandoEscopo() {
 console.log(`Y fora da função é: ${y}`);
 
 testandoEscopo();
+
+// 4 - escopo aninhado
+let m = 10;
+
+function escopoAninhado() {
+    let m = 20;
+
+    if(true) {
+        let m = 30;
+
+        if(true) {
+            let m = 40;
+
+            console.log(m);
+        }
+    }
+}
