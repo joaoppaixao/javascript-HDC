4defd3da32b87ba12741143c0d877cf995810bbd
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-01-17 12:04:44 -0300

    arrays e objetos

---
+++ 18_arrays_e_objetos/js/script.js
@@ -0,0 +1,36 @@
+// 1 - arrays
+const lista = [1, 2, 3, 4, 5]
+
+console.log(lista);
+
+const itens = ["João", true, 12, []];
+
+console.log(itens);
+
+// 2 - mais sobre arrays
+const arr = ["a", "b", "c", "d", "f"];
+
+console.log(arr[2]);
+
+// 3 - propriedades
+const numbers = [ 5, 3, 4];
+
+console.log(numbers.length);
+
+const myName = "João";
+
+console.log(myName.length);
+
+// 4 - métodos
+const otherNumbers = [1, 2, 3];
+
+const allNumbers = numbers.concat(otherNumbers);
+
+console.log(allNumbers);
+
+const text = "algum texto";
+
+console.log(text.toUpperCase());
+
+console.log(text.indexOf("t"));
+
