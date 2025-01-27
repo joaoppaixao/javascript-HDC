65377774015934f53068db391bb5b21d295a25aa
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-01-27 13:56:16 -0300

    finally em js

--- 20_debug/js/script.js
+++ 20_debug/js/script.js
@@ -87,3 +87,16 @@ try {
 } catch(error) {
     console.log(`Erro no programa: ${error}`)
 }
+
+// 7 - finally
+try {
+    const value = checkNumber("26")
+
+    if (!value) {
+        throw new Error("Valores inválidos!")
+    }
+} catch(error) {
+    console.log(`Opa, aconteceu um problema: ${error}`)
+} finally {
+    console.log("O código foi executado!")
+}
