3920c2c38fd2f6956bf65e04c9b19348858c163e
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-01-28 10:37:15 -0300

    atualizei o modulo de programação assíncrona

--- 21_assincrona/js/node_f453b1afe05a7.tmp.js
+++ 21_assincrona/js/node_f453b1afe05a7.tmp.js
@@ -10,8 +10,8 @@ console.log("Ainda não executou 2");
 // 2 - setInterval
 console.log("Ainda não começou");
 
-// setInterval(() => {
-//     console.log("Intervalo assíncrono")
-// }, 3000);
+setInterval(() => {
+    console.log("Intervalo assíncrono")
+}, 3000);
 
 console.log("Ainda não começou 2");
