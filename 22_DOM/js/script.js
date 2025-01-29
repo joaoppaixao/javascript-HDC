736073ff771cd60e74ecb547ad2512a22d50f45b
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-01-29 14:20:34 -0300

    atualizei a seção de DOM

--- 22_DOM/js/script.js
+++ 22_DOM/js/script.js
@@ -6,3 +6,27 @@ console.log(document.body.childNodes[1]);
 console.log(document.body.childNodes[1].childNodes);
 
 console.log(document.body.childNodes[1].childNodes[1].textContent);
+
+// 2 - selecionando por tag
+const listItens = document.getElementsByTagName("li");
+
+console.log(listItens);
+
+// 3 - selecionando por id
+const title = document.getElementById("title");
+
+console.log(title);
+
+// 4 - selecionando elementos por classe
+const products = document.getElementsByClassName("product");
+
+console.log(products);
+
+// 5 - selecionando os elementos por css
+const productsQuery = document.querySelectorAll(".product");
+
+console.log(productsQuery);
+
+const mainContainer = document.querySelector("#main-container");
+
+console.log(mainContainer);
