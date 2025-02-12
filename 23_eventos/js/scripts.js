07800acfd3611a9d40750b6a121f65935316c86f
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-02-12 15:32:37 -0300

    iniciei a seção de eventos

---
+++ 23_eventos/js/scripts.js
@@ -0,0 +1,23 @@
+// 1 - adicionando eventos
+const btn = document.querySelector("#my-button");
+
+btn.addEventListener("click", function() {
+    console.log("Cliclou aqui!");
+})
+
+// 2 - removendo evento
+const secondBtn = document.querySelector("#btn")
+
+function imprimirMensagem() {
+    console.log("Teste")
+}
+
+secondBtn.addEventListener("click", imprimirMensagem);
+
+const thirdBtn = document.querySelector("#other-btn")
+
+thirdBtn.addEventListener("click", () => {
+
+    console.log("Evento removido")
+    secondBtn.removeEventListener("click", imprimirMensagem)
+})
