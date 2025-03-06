19b0927b87944edffe2064848c4f697f1c5c0dcc
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-03-06 15:19:22 -0300

    módulo de eventos finalizado

--- 23_eventos/js/scripts.js
+++ 23_eventos/js/scripts.js
@@ -73,3 +73,52 @@ document.addEventListener("mousemove", (e) => {
     // console.log(`No eixo X: ${e.x}`)
     // console.log(`No eixo Y: ${e.y}`)
 })
+
+// 9 - evento de scroll
+window.addEventListener("scroll", (e) => {
+    if(window.pageYOffset > 200) {
+        console.log("Passamos de 200px")
+    }
+})
+
+// 10 - eventos de focus/blur
+
+const input = document.querySelector("#my-input");
+
+input.addEventListener("focus", (e) => {
+    console.log("Entrou no input.")
+})
+
+input.addEventListener("blur", (e) => {
+    console.log("Saiu do input.")
+})
+
+// 11 - evento de carregamento
+
+window.addEventListener("load", () => {
+    console.log("A página carregou!")
+})
+
+window.addEventListener("beforeunload", (e) => {
+    e.preventDefault();
+})
+
+// 12 - debounce
+const debounce = (f, delay) => {
+    let timeout 
+
+    return (...arguments) => {
+        if(timeout) {
+            clearTimeout(timeout)
+        }
+
+        timeout = setTimeout(() => {
+            f.apply(arguments);
+        }, delay)
+    }
+}
+
+window.addEventListener("mousemove", debounce(() => {
+    console.log("Executando a cada 400ms")
+}, 400)
+);
