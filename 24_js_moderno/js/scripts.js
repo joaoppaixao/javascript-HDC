973bc7c1da07a2dee4e2840c9a717b823cf6d80d
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-09-09 15:56:59 -0300

    iniciando a seção de JS ES6+

---
+++ 24_js_moderno/js/scripts.js
@@ -0,0 +1,21 @@
+// 1 - var, let e const 
+var x = 10;
+var y = 15; 
+
+if (y > 10) {
+    var x = 5; 
+    console.log(x);
+}
+
+console.log(x);
+
+// com o let
+let a = 10;
+let b = 15;
+
+if (b > 10) {
+    let a = 5; 
+    console.log(a);
+}
+
+console.log(a);
