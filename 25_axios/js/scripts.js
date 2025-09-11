54c48df6d745521bd9c12cec49f0e59e042855c8
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-09-11 15:42:58 -0300

    requisição de POST em JS

--- 25_axios/js/scripts.js
+++ 25_axios/js/scripts.js
@@ -14,7 +14,7 @@ const getData = async() => {
                 headers: {
                     "Content-Type": "application/json",
                     custom: "header",
-                    
+
                 }
             }
         )
@@ -60,3 +60,17 @@ const printData = async() => {
 
 printData();
 
+// 5 - post
+const form = document.querySelector("#post-form");
+const titleInput = document.querySelector("#title");
+const bodyInput = document.querySelector("#body");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    axios.post('https://jsonplaceholder.typicode.com/posts', {
+        title: titleInput.value, 
+        body: bodyInput.value, 
+        userId: 1,
+    })
+});
